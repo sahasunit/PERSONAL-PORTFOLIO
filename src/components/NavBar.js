2e79dcd602e45c0e21fcfa1c0eb0e2f88314565e
Sunit Saha <sunitsaha@Sunits-Mac.local>
2024-08-20 17:05:36 +0930

    deploy to github pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,58 +30,56 @@ export const NavBar = () => {
     }
 
     return (
-        <Router>
-            <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-                <Container >
-                    <Navbar.Brand href="#/" className="logo">
-                        <h1>Designed by Sunit</h1>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" >
-                        <span className="navbar-toggler-icon"></span>
-                    </Navbar.Toggle>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link 
-                            href="#home" 
-                            className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
-                            onClick = {() => onUpdateActiveLink('home')}
-                            >
-                                Home
-                            </Nav.Link>
-                            <Nav.Link 
-                            href="#skills" 
-                            className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-                            onClick = {() => onUpdateActiveLink('skills')}
-                            >
-                                Skills
-                            </Nav.Link>
-                            <Nav.Link 
-                            href="#workExperience" 
-                            className={activeLink === 'workExperience' ? 'active navbar-link' : 'navbar-link'}
-                            onClick = {() => onUpdateActiveLink('workExperience')}
-                            >
-                                Work Experience
-                            </Nav.Link>
-                            <Nav.Link 
-                            href="#artworks" 
-                            className={activeLink === 'artworks' ? 'active navbar-link' : 'navbar-link'}
-                            onClick = {() => onUpdateActiveLink('artworks')}
-                            >
-                                Artworks
-                            </Nav.Link>
-                        </Nav>
-                        <span className="navbar-text">
-                            <div className="social-icon">
-                                <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sunitsaha-99/"><img src={navIcon1} alt="LinkedIn Logo"/></a>
-                                <a rel="noreferrer" target="_blank" href="https://github.com/sahasunit"><img src={navIcon2} alt="Github Logo"/></a>
-                            </div>
-                            <HashLink to='#connect'>
-                                <button className="vvd"><span>Let’s Connect</span></button>
-                            </HashLink>
-                        </span>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </Router>
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+            <Container>
+                <Navbar.Brand href="#/" className="logo">
+                    <h1>Designed by Sunit</h1>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link 
+                        href="#home" 
+                        className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => onUpdateActiveLink('home')}>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link 
+                        href="#skills" 
+                        className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => onUpdateActiveLink('skills')}>
+                            Skills
+                        </Nav.Link>
+                        <Nav.Link 
+                        href="#workExperience" 
+                        className={activeLink === 'workExperience' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => onUpdateActiveLink('workExperience')}>
+                            Work Experience
+                        </Nav.Link>
+                        <Nav.Link 
+                        href="#artworks" 
+                        className={activeLink === 'artworks' ? 'active navbar-link' : 'navbar-link'}
+                        onClick={() => onUpdateActiveLink('artworks')}>
+                            Artworks
+                        </Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/sunitsaha-99/">
+                                <img src={navIcon1} alt="LinkedIn Logo" />
+                            </a>
+                            <a rel="noreferrer" target="_blank" href="https://github.com/sahasunit">
+                                <img src={navIcon2} alt="Github Logo" />
+                            </a>
+                        </div>
+                        <HashLink to="#connect">
+                            <button className="vvd"><span>Let’s Connect</span></button>
+                        </HashLink>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
